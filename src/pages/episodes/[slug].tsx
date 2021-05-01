cf66702b51aaa101cdf2f15e2ef9588bb61485a0
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2021-05-01 20:30:37 -0300

    ⚡️ added Incremental static regeneration

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -60,11 +60,29 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+   const { data } = await api.get('episodes', {
+      params: {
+         _limit: 2,
+         _sort: 'published_at',
+         _order: 'desc'
+      }
+   })
+
+   const paths = data.map(episode => {
+      return {
+         params: {
+            slug: episode.id
+         }
+      }
+   })
+
    return {
-      paths: [],
+      paths,
       fallback: 'blocking'
    }
-} 
+
+   // Incremental static regeneration
+}  
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
    const { slug } = ctx.params;
@@ -86,7 +104,6 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
    return {
       props: {
          episode,
-
       },
       revalidate: 60 * 60 * 24 // 24 hours
    }
